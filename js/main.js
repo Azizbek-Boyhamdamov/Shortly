bb923fbb4121c2c73e1fd1d5d8cdcee6b804fab9
Azizbek-Boyhamdamov <boyhamdamovazizbek27@gmail.com>
2022-06-11 21:54:07 +0500

    Add form-fields autofill styles

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ const elHeaderMenu = document.querySelector('.site-header__menu');
 elHeaderMenu.addEventListener('click',function(){
   elMenu.classList.toggle('menu-opener');
 });
+
+const elUrlShortenerForm = document.querySelector( '.js-url-shortener-form');
+const elUrlShortenerResults = document.querySelector('.url-shortener__results');
+
+elUrlShortenerForm.addEventListener('submit', function(evt){
+  evt.preventDefault();
+
+  elUrlShortenerResults.classList.add('url-shortener__results--open');
+});
+
+window.addEventListener('click',function(){
+  elUrlShortenerResults.classList.remove('url-shortener__results--open');
+});
