3f7ab71bc29fcba12a884a5ec9b8e45ed6cbb9be
Azizbek-Boyhamdamov <boyhamdamovazizbek27@gmail.com>
2022-06-11 22:49:07 +0500

    Add url copy button click handler .Wrap form submit handler in if condition

--- js/main.js
+++ js/main.js
@@ -1,19 +1,50 @@
 const elMenu = document.querySelector('.menu');
 const elHeaderMenu = document.querySelector('.site-header__menu');
+const elUrlShortener = document.querySelector('.url-shortener');
+const elUrlShortenerForm = document.querySelector( '.js-url-shortener-form');
+const elUrlShortenerResults = document.querySelector('.url-shortener__results');
+const elCopyUrlShortLinkButton = document.querySelector('.js-copy-short-link-button');
+
+// Menu event listener
 
 elHeaderMenu.addEventListener('click',function(){
   elMenu.classList.toggle('menu-opener');
 });
 
-const elUrlShortenerForm = document.querySelector( '.js-url-shortener-form');
-const elUrlShortenerResults = document.querySelector('.url-shortener__results');
 
-elUrlShortenerForm.addEventListener('submit', function(evt){
-  evt.preventDefault();
+// Url Shortener event listener
 
-  elUrlShortenerResults.classList.add('url-shortener__results--open');
-});
+if (elUrlShortenerForm){
+  elUrlShortenerForm.addEventListener('submit', function(evt){
+    evt.preventDefault();
 
-window.addEventListener('click',function(){
-  elUrlShortenerResults.classList.remove('url-shortener__results--open');
-});
+    elUrlShortenerResults.classList.add('url-shortener__results--open');
+  });
+}
+
+if(elUrlShortener){
+  elUrlShortener.addEventListener("click", function(evt){
+    if(evt.target.matches('.js-copy-short-link-button')){
+      // Change button content
+      evt.target.textContent = "Copied!";
+
+      // Change button bg-color
+
+      evt.target.classList.add('url-shortener__copy-btn--copied');
+
+      // Reset button text and class after 1 second
+
+          setTimeout(()=>{
+            evt.target.textContent = 'Copy';
+            evt.target.classList.remove ('url-shortener__copy-btn--copied');
+            },2000);
+
+    }
+  });
+}
+// Window event listener
+
+// window.addEventListener('click',function(){
+//   elUrlShortenerResults.classList.remove('url-shortener__results--open');
+// });
+
